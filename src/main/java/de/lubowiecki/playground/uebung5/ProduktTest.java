dab0c8303e19db7601386f757fe7600befa59849
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2024-03-12 16:34:10 +0100

    Produkte

---
+++ src/main/java/de/lubowiecki/playground/uebung5/ProduktTest.java
@@ -0,0 +1,39 @@
+package de.lubowiecki.playground.uebung5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProduktTest {
+
+    public static void main(String[] args) {
+
+        List<Produkt> produkte = new ArrayList<>();
+
+        char auswahl = 'n'; // 'v';
+
+        Produkt p;
+
+        if(auswahl == 'v') {
+            VerderblichesProdukt vp = new VerderblichesProdukt();
+            // Ablaufdatum befüllen
+            p = vp;
+        }
+        else {
+            p = new Produkt();
+        }
+        // befüllen
+
+        produkte.add(p);
+
+
+
+    }
+}
+
+class Produkt {
+
+}
+
+class VerderblichesProdukt extends Produkt {
+
+}
